c84a7e78311662cf6347e11acdef38750b264df0
Anwesh43 <anweshthecool0@gmail.com>
2019-06-05 10:11:39 +0530

    adding constants and libraries

--- linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
+++ linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.linesmallarcview
 /**
  * Created by anweshmishra on 05/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val nodes : Int = 5
+val lines : Int = 4
+val rotDeg : Float = 30f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
