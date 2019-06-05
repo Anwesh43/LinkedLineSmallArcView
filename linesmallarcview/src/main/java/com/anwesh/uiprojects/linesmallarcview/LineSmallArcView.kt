f36d8b4f1acbb32df0f30a3e879eb2c6d5e1b378
Anwesh43 <anweshthecool0@gmail.com>
2019-06-05 11:08:14 +0530

    adding static create method

--- linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
+++ linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
@@ -215,4 +215,13 @@ class LineSmallArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineSmallArcView {
+            val view : LineSmallArcView = LineSmallArcView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
