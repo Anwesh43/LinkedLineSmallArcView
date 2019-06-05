22ceb637498e8d94e3f03e9d4b4540cb8efa53e4
Anwesh43 <anweshthecool0@gmail.com>
2019-06-05 10:47:15 +0530

    adding LSANode class

--- linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
+++ linesmallarcview/src/main/java/com/anwesh/uiprojects/linesmallarcview/LineSmallArcView.kt
@@ -124,4 +124,48 @@ class LineSmallArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSANode(var i : Int, val state : State = State()) {
+
+        private var next : LSANode? = null
+        private var prev : LSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdadting(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSANode {
+            var curr : LSANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
